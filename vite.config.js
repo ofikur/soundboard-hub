dad4563f4b1db739c6c4376ad3270558bbd2ef22
Ofikur R. <ofikurr@gmail.com>
2025-08-27 21:40:47 +0700

    fix error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://myinstants-api.vercel.app',
-        changeOrigin: true,oxy
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     }
